633cb853573827da1ee90f60f35a0289448f915b
Waseem Wasaya <waseem.wasaya@outlook.com>
2021-04-16 01:23:29 +0300

    Initial commit

---
+++ GrooveVinyles/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GrooveVinyles.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
